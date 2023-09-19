a4931e9b0df09c5dcd11c68b2d6ac3cd1411b1ad
hcfgod <80559377+hcfgod@users.noreply.github.com>
2023-09-18 18:57:40 -0600

    Add files via upload

--- WeaponSystem/Scripts/BaseGun.cs
+++ WeaponSystem/Scripts/BaseGun.cs
@@ -7,17 +7,69 @@ public enum EProjectileType { Raycast, Projectile };
 	
 public class BaseGun : BaseWeapon
 {
-
 	public EFireMode FireMode = EFireMode.Single;
 	public EProjectileType ProjectileType = EProjectileType.Projectile;
-	
+
+	protected IProjectileType _projectileType;
+	protected IFiringMode _firingMode;
+
+	private void Awake()
+	{
+		InitializeProjectileType();
+		InitializeFiringMode();
+	}
+
 	public override void Attack()
 	{
 		Shoot();
 	}
-	
+
 	public virtual void Shoot()
 	{
-		
+		_firingMode.ExecuteFiringSequence(_projectileType);
+	}
+
+	public void SwitchFireMode(EFireMode newFireMode)
+	{
+		FireMode = newFireMode;
+		InitializeFiringMode();
+	}
+
+	private void InitializeProjectileType()
+	{
+		// Initialize ProjectileType based on the enum
+		switch (ProjectileType)
+		{
+			case EProjectileType.Projectile:
+				_projectileType = GetComponent<PhysicalProjectile>();
+				break;
+			case EProjectileType.Raycast:
+				_projectileType = GetComponent<RaycastProjectile>();
+				break;
+		}
+	}
+
+	private void InitializeFiringMode()
+	{
+		// Remove the old firing mode component if it exists
+		if (_firingMode != null)
+		{
+			Destroy(_firingMode as MonoBehaviour);
+		}
+
+		// Initialize FiringMode based on the enum
+		switch (FireMode)
+		{
+		case EFireMode.Auto:
+			//_firingMode = gameObject.AddComponent<Automatic>();
+			break;
+		case EFireMode.Single:
+			_firingMode = gameObject.AddComponent<SingleShot>();
+			break;
+		case EFireMode.Burst:
+			//_firingMode = gameObject.AddComponent<Burst>();
+			break;
+			// Add more cases as you implement more firing modes
+		}
 	}
 }
--- WeaponSystem/Scripts/RangedWeapon.cs
+++ WeaponSystem/Scripts/RangedWeapon.cs
@@ -2,42 +2,5 @@
 
 public class RangedWeapon : BaseGun
 {
-	private IProjectileType _projectileType;
-	private IFiringMode _firingMode;
-
-	private void Awake()
-	{
-		_projectileType = GetComponent<IProjectileType>();
-		
-		switch(ProjectileType)
-		{
-			case EProjectileType.Projectile:
-				_projectileType = GetComponent<PhysicalProjectile>();
-			break;
-			
-			case EProjectileType.Raycast:
-				_projectileType = GetComponent<RaycastProjectile>();
-			break;
-		}
-		
-		switch(FireMode)
-		{
-			case EFireMode.Auto:
-				_firingMode = GetComponent<SingleShot>();
-			break;
-				
-			case EFireMode.Single:
-				_firingMode = GetComponent<SingleShot>();
-			break;
-				
-			case EFireMode.Burst:
-				_firingMode = GetComponent<SingleShot>();
-			break;
-		}
-	}
-
-	public override void Shoot()
-	{
-		_firingMode.ExecuteFiringSequence(_projectileType);
-	}
+	
 }
